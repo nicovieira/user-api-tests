1fe1dd774f60105a771f3f52b79c8c875895ee1d
nicovieira <nicolasmvieira@protonmail.com>
2024-07-30 14:37:11 -0300

    parte 1

--- src/__tests__/__mocks__/user.mocks.ts
+++ src/__tests__/__mocks__/user.mocks.ts
@@ -1,23 +1,30 @@
 import jwt from "jsonwebtoken";
 import { prisma } from "../../database/prisma";
+import bcrypt from "bcrypt";
 
 export const userMock = {
-  id: "855ae363-9e01-483e-a6d4-1a20d64fa762",
+  id: "a2009ea6-5105-5ef5-9c21-39898f1f0d09",
   name: "John Doe",
   email: "johndoe@email.com",
 };
 
-export const completeUserMock = {
-  id: "855ae363-9e01-483e-a6d4-1a20d64fa762",
-  name: "John Doe",
-  email: "johndoe@email.com",
-  password: "12345678",
+export const completeUserMock = async () => {
+  const hashPassword = await bcrypt.hash("fb7c849KD", 10);
+  return {
+    id: "a2009ea6-5105-5ef5-9c21-39898f1f0d09",
+    name: "John Doe",
+    email: "johndoe@email.com",
+    password: hashPassword,
+  };
 };
-
 export const userRegisterBodyMock = {
   name: "John Doe",
   email: "johndoe@email.com",
-  password: "12345678",
+  password: "fb7c849KD",
+};
+export const userLoginBodyMock = {
+  email: "johndoe@email.com",
+  password: "fb7c849KD",
 };
 
 export const loginUserMock = async () => {
